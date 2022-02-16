f6b9045c83bdb4bf027e7715970e9f859e2b806c
Allochka11 <allavelychko11@gmail.com>
2022-02-16 12:34:11 +0200

    Making a structure of project

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+
+    return (
+        <header className="header">
+            <img src="https://www.pikpng.com/pngl/m/481-4815508_vkontakte-logo-png-vk-png-clipart.png"
+                 alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Profile() {
+
+    return (
+        <div className="content">
+            <div>
+                <img src="https://www.searchenginejournal.com/wp-content/uploads/2018/10/How-to-Boost-Your-Images%E2%80%99-Visibility-on-Google-Images-760x400.png" alt=""/>
+            </div>
+            <div>
+                <img src="https://cdn.jpegmini.com/user/images/slider_puffin_before_mobile.jpg" alt=""/>
+                <div>desc</div>
+            </div>
+            <div>
+                my posts
+                <div>New post</div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
