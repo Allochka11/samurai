cd66961e6e347478300012c43deb51584066ae12
Allochka11 <allavelychko11@gmail.com>
2022-11-21 19:55:20 +0200

    lesson 53-54 - React component, lifecycle methods

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,81 +1,46 @@
 import React from "react";
+import userPhoto from "../../assets/images/images.png";
+import s from "./Users.module.css";
+import axios from "axios";
 import {UsersMapStateDispatchPropsTypes} from "./UsersContainer";
-import s from '../Users/Users.module.css'
 
 
-export const Users = (props: UsersMapStateDispatchPropsTypes) => {
-
-    if (props.users.length === 0) {
-        props.setUsers([{
-            id: 1,
-            photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-            followed: true,
-            fullName: 'Alla',
-            status: 'Lalalla',
-            location: {city: 'Kyiv', country: 'Ukraine'}
-        },
-            {
-                id: 2,
-                photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-                followed: false,
-                fullName: 'Ann',
-                status: 'Lalalla',
-                location: {city: 'Krakow', country: 'Poland'}
-            },
-            {
-                id: 3,
-                photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-                followed: true,
-                fullName: 'Alister',
-                status: 'Lalalla',
-                location: {city: 'London', country: 'GB'}
-            },
-            {
-                id: 4,
-                photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-                followed: false,
-                fullName: 'Pol',
-                status: 'Lalalla',
-                location: {city: 'Berlin', country: 'Germany'}
-            },
-            {
-                id: 5,
-                photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-                followed: true,
-                fullName: 'Alla',
-                status: 'Lalalla',
-                location: {city: 'Cietl', country: 'Australia'}
-            }])
+class Users extends React.Component<UsersMapStateDispatchPropsTypes> {
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                console.log(response.data.items);
+                this.props.setUsers(response.data.items);
+            })
     }
 
+    render() {
 
-    let a = props.users.map(el => <div key={el.id}>
-        <span>
-            <div><img src={el.photoUrl} alt="" className={s.userPhoto}/></div>
-            <div>
-                {el.followed
-                    ? <button onClick={() => props.unfollow(el.id)}>Unfollow</button>
-                    : <button onClick={() => props.follow(el.id)}>Follow</button>
-                }
-            </div>
-        </span>
-        <span>
-            <span>
-                <div>{el.fullName}</div>
-                <div>{el.status}</div>
-            </span>
-            <span>
-                <div>{el.location.city}</div>
-                <div>{el.location.country}</div>
-            </span>
-        </span>
-    </div>)
-
-    return (
-        <div>
-            {a}
-            Im superUser
-
+        return <div>
+            {this.props.users.map((el) => (<div key={el.id}>
+                <span>
+                    <div><img src={el.photos.small !== null ? el.photos.small : userPhoto} alt=""
+                              className={s.userPhoto}/></div>
+                    <div>
+                        {el.followed
+                            ? <button onClick={() => this.props.unfollow(el.id)}>Unfollow</button>
+                            : <button onClick={() => this.props.follow(el.id)}>Follow</button>
+                        }
+                    </div>
+                </span>
+                <span>
+                    <span>
+                    <div>{el.name}</div>
+                    <div>{el.status}</div>
+                </span>
+                <span>
+                    <div>{'el.location.city'}</div>
+                    <div>{'el.location.country'}</div>
+                </span>
+                </span>
+            </div>))}
         </div>
-    );
-};
+    }
+}
+
+export default Users;
--- src/redux/users-reducer.tsx
+++ src/redux/users-reducer.tsx
@@ -9,12 +9,16 @@ type LocationPropsType = {
     city: string
     country: string
 }
+type PhotosType = {
+    small: string
+    large: string
+}
 
 export type UsersPropsType = {
+    photos: PhotosType,
     id: number
-    photoUrl: string
     followed: boolean
-    fullName: string
+    name: string
     status: string
     location: LocationPropsType
 }
@@ -26,49 +30,7 @@ export type UsersPagePropsType = {
 }
 
 let initialState: UsersPagePropsType = {
-    users: [
-        // {
-        //     id: 1,
-        //     photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-        //     followed: true,
-        //     fullName: 'Alla',
-        //     status: 'Lalalla',
-        //     location: {city: 'Kyiv', country: 'Ukraine'}
-        // },
-        // {
-        //     id: 2,
-        //     photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-        //     followed: false,
-        //     fullName: 'Ann',
-        //     status: 'Lalalla',
-        //     location: {city: 'Krakow', country: 'Poland'}
-        // },
-        // {
-        //     id: 3,
-        //     photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-        //     followed: true,
-        //     fullName: 'Alister',
-        //     status: 'Lalalla',
-        //     location: {city: 'London', country: 'GB'}
-        // },
-        // {
-        //     id: 4,
-        //     photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-        //     followed: false,
-        //     fullName: 'Pol',
-        //     status: 'Lalalla',
-        //     location: {city: 'Berlin', country: 'Germany'}
-        // },
-        // {
-        //     id: 5,
-        //     photoUrl: 'https://www.president.gov.ua/storage/j-image-storage/14/45/33/7d316e643ff52bc8c3591c1c005174c2_1574167504_extra_large.jpeg',
-        //     followed: true,
-        //     fullName: 'Alla',
-        //     status: 'Lalalla',
-        //     location: {city: 'Cietl', country: 'Australia'}
-        // },
-    ]
-
+    users: []
 }
 
 export const usersReducer = (state: UsersPagePropsType = initialState, action: ActionsTypes): UsersPagePropsType => {
